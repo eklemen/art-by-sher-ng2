0798b229747656cffe5a841f06b6dd95996b2559
Evan Klemen <ejklemen@gmail.com>
2016-08-07 19:09:06 -0400

    added bootstrap and angular2-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {AlertComponent, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 import { ApiService } from './shared';
 
@@ -12,13 +13,13 @@ import '../style/app.scss';
 @Component({
   selector: 'my-app', // <my-app></my-app>
   providers: [ApiService],
-  directives: [...ROUTER_DIRECTIVES],
+  directives: [...ROUTER_DIRECTIVES, AlertComponent, DATEPICKER_DIRECTIVES],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   url = 'https://github.com/preboot/angular2-webpack';
-
+  date: Date = new Date();
   constructor(private api: ApiService) {
   }
 }
